0d3c12f173c2291679f692838a655be97bec525e
Michael Lobato <mike.lobato.wft@gmail.com>
2022-09-23 06:57:24 -0700

    Initial commit

---
+++ codeChallengeMild.js
@@ -0,0 +1,11 @@
+//Mild
+
+//Write a JavaScript program that defines a variable that stores an Number.
+//The program should print out the String "even" if the Number is even, and the String "odd" if the Number is odd.
+var cookies = 11;
+  if (cookies % 2 === 0) {
+  console.log ("even");
+}
+  else if (cookies % 2 === 1) {
+console.log ("odd");
+}
